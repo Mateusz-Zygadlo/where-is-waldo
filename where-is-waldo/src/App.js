485fbd020493012deb65b8076912b4c69ddc7fff
Mateusz <mateuszzygadlo00@gmail.com>
2021-10-10 18:58:52 +0200

    Adding firebase

--- where-is-waldo/src/App.js
+++ where-is-waldo/src/App.js
@@ -1,7 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyDum1S9rK1r7PVi3nhybg_7DkznOdYf5EQ",
+  authDomain: "where-s-waldo-77974.firebaseapp.com",
+  projectId: "where-s-waldo-77974",
+  storageBucket: "where-s-waldo-77974.appspot.com",
+  messagingSenderId: "607103542662",
+  appId: "1:607103542662:web:721f84db67270edfadf807"
+})
+
+const db = getFirestore(firebaseApp);
+
 const App = () => {
   return(
     <div>
-      <h1>Hello World</h1>
+      <h1>hello world</h1>
     </div>
   )
 }
